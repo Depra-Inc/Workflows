d9d4c4374d4c84bdca92ddd852e0b679c17ab849
godzzz <g0dzZz1lla@yandex.ru>
2023-11-17 03:19:11 +0400

    Update publish-gpr workflow

---
+++ .github/workflows/patch-dependencies.js
@@ -0,0 +1,18 @@
+﻿const fs = require("fs");
+
+const patchDependencies = (scope) => {
+    const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+    packageJson.dependencies = Object.fromEntries(
+        Object.entries(packageJson.dependencies).map(([key, value]) => {
+            if (key.startsWith("com.depra")) {
+                return [["@${scope}/", key].join(""), value];
+            } else {
+                return [key, value];
+            }
+        })
+    );
+    fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2));
+    console.log(JSON.stringify(packageJson.dependencies, null, 2));
+};
+
+module.exports = patchDependencies;
